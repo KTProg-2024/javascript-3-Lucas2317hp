d37c74a741790f21698508459e646016713de803
Lucas2317 <>
2021-06-07 17:25:08 +0000

    done

--- motion.js
+++ motion.js
@@ -4,11 +4,26 @@ function myMove() {
   var posLeft = 0;
   var id = setInterval(frame, 5);
   function frame() {
-	  posTop++;
-	  posLeft++;
-	  elem.style.top = posTop + "px"; 
-	  elem.style.left = posLeft + "px"; 
-	  if ((posTop == 350) && (posLeft == 350))
-		  clearInterval(id);
+    posTop++;
+    posLeft++;
+    elem.style.top = posTop + "px"; 
+    elem.style.left = posLeft + "px"; 
+    if ((posTop == 350) && (posLeft == 350))
+      clearInterval(id);
+  }
+}
+
+function blue() {
+  var elem = document.getElementById("bluesquare");   
+  var posTop = 0;
+  var posRight = 0;
+  var id = setInterval(frame, 5);
+  function frame() {
+    posTop++;
+    posRight++;
+    elem.style.top = posTop + "px"; 
+    elem.style.right = posRight + "px"; 
+    if ((posTop == 350) && (posRight == 350))
+      clearInterval(id);
   }
 }
